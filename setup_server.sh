6a2e0d4bc43ce40f436dd6e40473d7d09339a095
gear259 <51294291+gear259@users.noreply.github.com>
2019-07-06 00:43:56 +0100

    Update setup_server.sh

--- setup_server.sh
+++ setup_server.sh
@@ -5,5 +5,5 @@ git submodule update --init --recursive && cd build
 git clone https://github.com/fastogt/pyfastogt && cd pyfastogt
 python3 setup.py install && cd ../
 rm -rf pyfastogt
-./iptv/build/build_env.py && echo "build finished! Get your key"
+./iptv/build/build_env.py && echo "build finished! Your licence key is below. Copy it to a notepad and also append to next command"
 license_gen
