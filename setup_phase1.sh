9b102ba8e5b329428e3604cdb5af9abdc754f3d7
gear259 <51294291+gear259@users.noreply.github.com>
2019-07-06 01:58:13 +0100

    Update setup_phase1.sh

--- setup_phase1.sh
+++ setup_phase1.sh
@@ -1,9 +1,82 @@
+
 #! /bin/sh
-cd ../ && apt-get update && apt-get upgrade && apt-get install python3 python3-setuptools git
-git clone https://github.com/fastogt/iptv && cd iptv
-git submodule update --init --recursive && cd build
-git clone https://github.com/fastogt/pyfastogt && cd pyfastogt
-python3 setup.py install && cd ../
+function delay()
+{
+    sleep 0.2;
+}
+
+#
+# Description : print out executing progress
+# 
+CURRENT_PROGRESS=0
+function progress()
+{
+    PARAM_PROGRESS=$1;
+    PARAM_PHASE=$2;
+
+    if [ $CURRENT_PROGRESS -le 0 -a $PARAM_PROGRESS -ge 0 ]  ; then echo -ne "[..........................] (0%)  $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 5 -a $PARAM_PROGRESS -ge 5 ]  ; then echo -ne "[#.........................] (5%)  $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 10 -a $PARAM_PROGRESS -ge 10 ]; then echo -ne "[##........................] (10%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 15 -a $PARAM_PROGRESS -ge 15 ]; then echo -ne "[###.......................] (15%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 20 -a $PARAM_PROGRESS -ge 20 ]; then echo -ne "[####......................] (20%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 25 -a $PARAM_PROGRESS -ge 25 ]; then echo -ne "[#####.....................] (25%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 30 -a $PARAM_PROGRESS -ge 30 ]; then echo -ne "[######....................] (30%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 35 -a $PARAM_PROGRESS -ge 35 ]; then echo -ne "[#######...................] (35%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 40 -a $PARAM_PROGRESS -ge 40 ]; then echo -ne "[########..................] (40%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 45 -a $PARAM_PROGRESS -ge 45 ]; then echo -ne "[#########.................] (45%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 50 -a $PARAM_PROGRESS -ge 50 ]; then echo -ne "[##########................] (50%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 55 -a $PARAM_PROGRESS -ge 55 ]; then echo -ne "[###########...............] (55%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 60 -a $PARAM_PROGRESS -ge 60 ]; then echo -ne "[############..............] (60%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 65 -a $PARAM_PROGRESS -ge 65 ]; then echo -ne "[#############.............] (65%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 70 -a $PARAM_PROGRESS -ge 70 ]; then echo -ne "[###############...........] (70%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 75 -a $PARAM_PROGRESS -ge 75 ]; then echo -ne "[#################.........] (75%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 80 -a $PARAM_PROGRESS -ge 80 ]; then echo -ne "[####################......] (80%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 85 -a $PARAM_PROGRESS -ge 85 ]; then echo -ne "[#######################...] (85%) $PARAM_PHASE \r"  ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 90 -a $PARAM_PROGRESS -ge 90 ]; then echo -ne "[##########################] (100%) $PARAM_PHASE \r" ; delay; fi;
+    if [ $CURRENT_PROGRESS -le 100 -a $PARAM_PROGRESS -ge 100 ];then echo -ne 'Done!                                            \n' ; delay; fi;
+
+    CURRENT_PROGRESS=$PARAM_PROGRESS;
+
+}
+
+#################################### INFO ######################################
+
+# This is the first script PHASE 1 for installing IPTV
+
+################################################################################
+
+echo "The task is in progress, please wait a few seconds"
+echo "going to download IPTV Phase 1 files from git"
+echo
+progress 0 "Initialize"
+cd /
+progress 10 "Prepare System"
+apt-get update && apt-get upgrade
+progress 15 "Phase 1 - Install Dependencies"
+apt-get install python3 python3-pip python3-setuptools git mongodb
+echo "Dependencies installed"
+progress 20 "Downloading iptv files from github"
+git clone https://github.com/fastogt/iptv
+progress 25 "Processing downloaded iptv files"
+cd iptv
+git submodule update --init --recursive
+echo "Success"
+progress 30 "Changing Directory"
+cd build
+progress 35 "Downloading pyfastogt from github"
+git clone https://github.com/fastogt/pyfastogt
+echo "Success"
+progress 40 "Processing pyfastogt files"
+cd pyfastogt
+python3 setup.py install
+echo "Success! Now deleting uneeded files"
+cd ../
 rm -rf pyfastogt
-./iptv/build/build_env.py && echo "build finished! Your licence key is below. Copy it to a notepad and also append to next command"
+progress 50 "Processing Build Package"
+echo "This will take a long time. If running, come back in 15 minutes and check on progress"
+./iptv/build/build_env.py
+progress 95 "Build Package Installed"
+echo "Did you fall asleep waiting?  Nearly done now, lets get the key"
+progress 100 "Fetching your key"
+echo "your unique license key is below. Copy it to a notepad and then append to next command"
 license_gen
