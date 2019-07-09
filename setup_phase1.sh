cc1fc7dfd2bdf9a08f818ae4e3459b09643ba0ad
gear259 <51294291+gear259@users.noreply.github.com>
2019-07-09 23:52:59 +0100

    Update setup_phase1.sh

--- setup_phase1.sh
+++ setup_phase1.sh
@@ -1,5 +1,9 @@
 
 #! /bin/sh
+
+#run as root
+set -ex
+#progress code
 function delay()
 {
     sleep 0.2;
@@ -38,7 +42,7 @@ function progress()
     CURRENT_PROGRESS=$PARAM_PROGRESS;
 
 }
-
+#end of progress code
 #################################### INFO ######################################
 
 # This is the first script PHASE 1 for installing IPTV
@@ -49,20 +53,38 @@ echo "The task is in progress, please wait a few seconds"
 echo "going to download IPTV Phase 1 files from git"
 echo
 progress 0 "Initialize"
-cd /
+
+# exports
+#export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
+
+# variables
+USER=iptv_cloud
+
+# update system and prepare dependencies
 progress 10 "Prepare System"
-apt-get update && apt-get upgrade
+apt-get update
 progress 15 "Phase 1 - Install Dependencies"
-apt-get install python3 python3-pip python3-setuptools git mongodb
+apt-get install -y git python3-setuptools python3-pip mongodb --no-install-recommends
 echo "Dependencies installed"
+
+#make the files
+cd /
+mkdir iptv_project
+cd iptv_project
 progress 20 "Downloading iptv files from github"
 git clone https://github.com/fastogt/iptv
+
+# sync modules
 progress 25 "Processing downloaded iptv files"
 cd iptv
 git submodule update --init --recursive
 echo "Success"
+
+#pre-build
 progress 30 "Changing Directory"
 cd build
+
+# install pyfastogt
 progress 35 "Downloading pyfastogt from github"
 git clone https://github.com/fastogt/pyfastogt
 echo "Success"
@@ -72,11 +94,55 @@ python3 setup.py install
 echo "Success! Now deleting uneeded files"
 cd ../
 rm -rf pyfastogt
+
+# build env for service
 progress 50 "Processing Build Package"
 echo "This will take a long time. If running, come back in 15 minutes and check on progress"
 ./iptv/build/build_env.py
-progress 95 "Build Package Installed"
+
+echo "Build Package Installed"
 echo "Did you fall asleep waiting?  Nearly done now, lets get the key"
-progress 100 "Fetching your key"
+
+echo "Fetching your key"
 echo "your unique license key is below. Copy it to a notepad and then append to next command"
-license_gen
+
+# build service with key
+LICENSE_KEY=$(license_gen)
+./build.py release $LICENSE_KEY
+
+# add user
+useradd -m -U -d /home/$USER $USER -s /bin/bash
+
+#phase2
+echo "going to download iptv_admin files from git"
+echo
+progress 60 "Initialize"
+cd /
+progress 70 "Phase 2"
+git clone https://github.com/fastogt/iptv_admin
+sleep 1
+echo "download complete to folder iptv_admin"
+progress 80 "processing files"
+cd iptv_admin
+git submodule update --init --recursive
+echo
+echo "Processing requirements"
+pip3 install -r requirements.txt
+#
+
+#creating admin user to log in to web panel
+progress 90 "Creating an admin user"
+./iptv_project/iptv_admin/scripts/create_provider.py --email=test@example.com --password=1234567
+
+
+progress 100 "iptv_admin panel ready to start"
+#
+echo
+echo
+echo
+echo "loading up IPTV Admin panel"
+echo "keep this console open"
+cd
+systemctl enable streamer_service.service
+systemctl start streamer_service.service
+./iptv_project/iptv_admin/server.py
